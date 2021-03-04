5ebc0849afe5270d23691992d818daf3aeb696ac
rafaelogayar <78820722+rafaelogayar@users.noreply.github.com>
2021-03-04 17:58:06 -0300

    V1.0
    Tradução MPU

--- MPU6050_DMP6_Projeto/MPU6050_DMP6_Projeto.ino
+++ MPU6050_DMP6_Projeto/MPU6050_DMP6_Projeto.ino
@@ -1,53 +1,28 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
-// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+// I2C device class (I2Cdev) esboço Arduino de demonstração para classe MPU6050 usando DMP (MotionApps v2.0)
+// 21/06/2012 por Jeff Rowberg <jeff@rowberg.net>
+// As atualizações devem (espero) estar sempre disponíveis em https://github.com/jrowberg/i2cdevlib
 //
 // Changelog:
-//      2019-07-08 - Added Auto Calibration and offset generator
-//		   - and altered FIFO retrieval sequence to avoid using blocking code
-//      2016-04-18 - Eliminated a potential infinite loop
-//      2013-05-08 - added seamless Fastwire support
-//                 - added note about gyro calibration
-//      2012-06-21 - added note about Arduino 1.0.1 + Leonardo compatibility error
-//      2012-06-20 - improved FIFO overflow handling and simplified read process
-//      2012-06-19 - completely rearranged DMP initialization code and simplification
-//      2012-06-13 - pull gyro and accel data from FIFO packet instead of reading directly
-//      2012-06-09 - fix broken FIFO read sequence and change interrupt detection to RISING
-//      2012-06-05 - add gravity-compensated initial reference frame acceleration output
-//                 - add 3D math helper file to DMP6 example sketch
-//                 - add Euler output and Yaw/Pitch/Roll output formats
-//      2012-06-04 - remove accel offset clearing for better results (thanks Sungon Lee)
-//      2012-06-01 - fixed gyro sensitivity to be 2000 deg/sec instead of 250
-//      2012-05-30 - basic DMP initialization working
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2012 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
+// 08/07/2019 - Adicionada calibração automática e gerador de deslocamento
+// - e sequência de recuperação FIFO alterada para evitar o uso de código de bloqueio
+// 18/04/2016 - Eliminado um loop infinito potencial
+// 2013-05-08 - adicionado suporte contínuo Fastwire
+// - nota adicionada sobre calibração do giroscópio
+// 2012-06-21 - adicionada nota sobre o Arduino 1.0.1 + erro de compatibilidade do Leonardo
+// 20/06/2012 - manipulação de estouro de FIFO aprimorada e processo de leitura simplificado
+// 2012-06-19 - código de inicialização DMP completamente reorganizado e simplificação
+// 2012-06-13 - extrair dados do giroscópio e aceleração do pacote FIFO em vez de ler diretamente
+// 2012-06-09 - conserta a sequência de leitura FIFO quebrada e altera a detecção de interrupção para RISING
+// 2012-06-05 - adicionar saída de aceleração do quadro de referência inicial compensada pela gravidade
+// - adicionar arquivo auxiliar de matemática 3D ao esboço de exemplo DMP6
+// - adicionar saída de Euler e formatos de saída Yaw / Pitch / Roll
+// 04/06/2012 - remover compensação de compensação de aceleração para obter melhores resultados (obrigado Sungon Lee)
+// 2012-06-01 - corrigiu a sensibilidade do giroscópio para 2.000 graus / s em vez de 250
+// 2012-05-30 - inicialização DMP básica funcionando
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
-#include <EEPROM.h>
 #include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
 
@@ -56,33 +31,13 @@ THE SOFTWARE.
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-
+ 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
 // AD0 high = 0x69
 MPU6050 mpu;
 //MPU6050 mpu(0x69); // <-- use for AD0 high
-
-/* =========================================================================
-   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-   depends on the MPU-6050's INT pin being connected to the Arduino's
-   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-   digital I/O pin 2.
- * ========================================================================= */
-
-/* =========================================================================
-   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
-   when using Serial.write(buf, len). The Teapot output uses this method.
-   The solution requires a modification to the Arduino USBAPI.h file, which
-   is fortunately simple, but annoying. This will be fixed in the next IDE
-   release. For more info, see these links:
-
-   http://arduino.cc/forum/index.php/topic,109987.0.html
-   http://code.google.com/p/arduino/issues/detail?id=958
- * ========================================================================= */
-
-
 
 // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
 // quaternion components in a [w, x, y, z] format (not best for parsing
@@ -114,8 +69,6 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
-
-
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
@@ -124,9 +77,6 @@ volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin h
 void dmpDataReady() {
     mpuInterrupt = true;
 }
-
-
-
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
@@ -219,7 +169,7 @@ void setup() {
 // ================================================================
 // ===                   Leitura e Gravação dos dados           ===
 // ================================================================
-
+ 
 void loop() {
     unsigned long currentMillis = millis();
     // if programming failed, don't try to do anything
@@ -238,7 +188,7 @@ void loop() {
             Serial.print("\t");
             Serial.println(q.z);
             Serial.print(currentMillis / 1000);
-            Serial.println(" segundos");
+            Serial.println("Segundos Decorridos");
             delay(250);
             
         #endif
